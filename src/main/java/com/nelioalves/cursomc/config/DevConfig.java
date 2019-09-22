e07f25324a57ee37bc970147fe9d471064aa8c99
Gustavo Gomes <gustavo.sgomess@gmail.com>
2019-09-22 15:55:55 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/nelioalves/cursomc/config/DevConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/DevConfig.java
@@ -1,12 +1,15 @@
 package com.nelioalves.cursomc.config;
 
 import java.text.ParseException;
+import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.nelioalves.cursomc.services.DBService;
 import com.nelioalves.cursomc.services.EmailService;
@@ -36,5 +39,23 @@ public class DevConfig {
 	@Bean
 	public EmailService emaiService() {
 		return new SmtpEmailService();
+	}
+	
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	     
+	    mailSender.setUsername("my.gmail@gmail.com");
+	    mailSender.setPassword("password");
+	     
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
+	     
+	    return mailSender;
 	}
 }
--- src/main/java/com/nelioalves/cursomc/config/TestConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/TestConfig.java
@@ -1,11 +1,14 @@
 package com.nelioalves.cursomc.config;
 
 import java.text.ParseException;
+import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.nelioalves.cursomc.services.DBService;
 import com.nelioalves.cursomc.services.EmailService;
@@ -27,5 +30,23 @@ public class TestConfig {
 	@Bean
 	public EmailService emaiService() {
 		return new MockEmailService();
+	}
+	
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	     
+	    mailSender.setUsername("my.gmail@gmail.com");
+	    mailSender.setPassword("password");
+	     
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
+	     
+	    return mailSender;
 	}
 }
